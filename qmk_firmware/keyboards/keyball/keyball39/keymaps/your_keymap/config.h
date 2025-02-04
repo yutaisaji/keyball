b475528a4b565201413fb189b94cda1bb2292817
yutaisaji <34624984+yutaisaji@users.noreply.github.com>
2025-02-04 23:08:36 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/your_keymap/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/your_keymap/config.h
@@ -39,7 +39,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define KEYBALL_SCROLLSNAP_ENABLE 0  // scrollsnapをオフにする
 
 #define TAP_CODE_DELAY 5
-#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
 
 #define KEYBALL_CPI_DEFAULT 700       // 光学センサーPMW3360DM の解像度 (CPI) の規定値
 #define KEYBALL_SCROLL_DIV_DEFAULT 4  // スクロール速度の規定値
