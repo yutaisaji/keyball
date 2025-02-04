a629f27e3c28fabbf3ca9315cfb1ad3d675af0c2
yutaisaji <34624984+yutaisaji@users.noreply.github.com>
2025-02-04 23:36:29 +0900

    Update rules.mk

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/your_keymap/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/your_keymap/rules.mk
@@ -2,4 +2,15 @@ RGBLIGHT_ENABLE = yes
 
 OLED_ENABLE = yes
 
-VIA_ENABLE = yes
+# VIA_ENABLE = yes
+
+
+### 使っていないもの
+### 以下はメモリ不足のため、VIAをOFFにする必要がある
+# KEY_OVERRIDE_ENABLE = yes
+COMBO_ENABLE = yes
+# AUTO_SHIFT_ENABLE = yes
+
+# ファームウェアのサイズを削減
+EXTRAFLAGS += -flto
+GRAVE_ESC_ENABLE = no  # 90バイト
